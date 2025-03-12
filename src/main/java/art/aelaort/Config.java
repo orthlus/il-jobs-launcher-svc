7b6bb74fc5ecf0d2b3d90713c78db523171af95d
ort <orthodoxplus@ya.ru>
2025-03-13 01:51:20 +0300

    k8s

--- src/main/java/art/aelaort/Config.java
+++ src/main/java/art/aelaort/Config.java
@@ -1,6 +1,8 @@
 package art.aelaort;
 
+import io.kubernetes.client.openapi.ApiClient;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -8,5 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableCaching
 public class Config {
-
+	@Bean
+	public ApiClient kubernetesApiClient() throws Exception {
+		ApiClient client = io.kubernetes.client.util.Config.defaultClient();
+		io.kubernetes.client.openapi.Configuration.setDefaultApiClient(client);
+		return client;
+	}
 }
